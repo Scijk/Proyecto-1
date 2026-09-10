7b2668e99734e604b6d492c45b85d8dd08b0d420
Chris Gomez <whoami@live.cl>
2026-09-09 21:17:22 -0300

    feat: testing

--- resources/views/proyectos/eliminar.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- filepath: resources/views/proyectos/eliminar.blade.php --}}
-@extends('layouts.app')
-
-@section('title', 'Eliminar Proyecto')
-
-@section('content')
-    <h2>Proyecto Eliminado (Simulado)</h2>
-    <p>El proyecto con ID <strong>{{ $id }}</strong> ha sido eliminado.</p>
-@endsection
